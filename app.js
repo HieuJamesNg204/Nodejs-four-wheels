6a974997d05352097d104bda9d62d0b43b23dce3
Hieu Nguyen <HieuJames204@gmail.com>
2024-10-22 16:37:20 +0700

    Update four wheels

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 import carRoutes from './routes/carRoutes.js';
 import automakerRoutes from './routes/automakerRoutes.js';
 import authRoutes from './routes/authRoutes.js';
@@ -8,6 +10,22 @@ import connectDB from './db.js';
 const app = express();
 
 connectDB();
+
+const corsOptions = {
+    origin: "http://example.com",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+};
+
+app.use(cors(corsOptions));
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 50,
+    message: 'The number of requests is becoming overwhelming, please try again after 5 minutes'
+});
+
+app.use(limiter);
 
 app.use(bodyParser.json());
 
